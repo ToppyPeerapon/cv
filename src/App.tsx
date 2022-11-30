16378fa6e4859de745202b20df0940f1b34e622a
Peerapon Chunpongthong <chun.peerapon@gmail.com>
2022-11-30 14:55:24 +0700

    Init detail on html

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,96 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import "./App.css"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div>
+      <div>
+        <h1>Peerapon Chunpongthong</h1>
+        <span>087-7486519</span>
+        <span>chun.peerapon@gmail.com</span>
+        <a href="https://github.com/ToppyPeerapon">github.com/ToppyPeerapon</a>
+        <a href="www.linkedin.com/in/peerapon-chunpongthong">
+          linkedin.com/peerapon-chunpongthong
         </a>
-      </header>
+      </div>
+
+      <div>
+        <h2>Experiences</h2>
+        <div>
+          <h3>Frontend Engineer (Web)</h3>
+          <h4>DF Marketplace Co., Ltd. (Kaidee)</h4>
+          <span>React.js, Typescript, Javascript, Cypress</span>
+          <span>January 2021 - November 2022</span>
+          <ul>
+            <li>Build web application with NextJs and strapi</li>
+            <li>Keep code test coverage with jest and E2E testing Cypress</li>
+            <li>
+              Focus on code quality with type on Typescript and code climate
+            </li>
+            <li>
+              Improved and support chat application in kaidee platform with
+              nest.js
+            </li>
+          </ul>
+        </div>
+
+        <div>
+          <h3>Software Engineer</h3>
+          <h4>Muang Thai Broker Co., Ltd. (Gettgo)</h4>
+          <span>
+            React.js, React-native, Javascript, Typescript, Cypress, Redux,
+            Redux-saga
+          </span>
+          <span>December 2019 - December 2022</span>
+          <ul>
+            <li>
+              Developed web application with React.js and Redux-saga for back
+              office Muang Thai Life Assurance Public Company Limited
+            </li>
+            <li>Developed Mobile Application with React-native Typescript</li>
+            <li>Focus on code quality with dynamic type, functional code</li>
+            <li>Keep code test coverage with jest and E2E testing Cypress</li>
+          </ul>
+        </div>
+
+        <div>
+          <h3>Frontend Developer</h3>
+          <h4>LQID 360 Co., Ltd.</h4>
+          <span>React.js, Javascript, Redux, Redux-Observerable</span>
+          <span>March 2019 - November 2019</span>
+          <ul>
+            <li>
+              Developed web application for back office with React.js and
+              Redux-observable
+            </li>
+            <li>Keep code test coverage with jest unit test</li>
+            <li>Use web socket for video call with webRTC</li>
+            <li>Use server-sent events for chat</li>
+          </ul>
+        </div>
+
+        <div>
+          <h3>Software Developer</h3>
+          <h4>MFEC Public Company Limited Co., Ltd.</h4>
+          <span>React.js, Redux, .NET Framework, Javascript</span>
+          <span>August 2018 - February 2019</span>
+          <ul>
+            <li>
+              Developed mobile application for back office in Muang Thai Life
+              Assurance with React.js and Cordova
+            </li>
+            <li>Improved and support application with Sencha.js</li>
+            <li>
+              Developed web application and mobile application for with Ionic
+            </li>
+            <li>
+              Developed web application for back office with .Net Framework
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
